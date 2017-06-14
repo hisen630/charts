e66f4adba8d31d44340ad96df16b58ff1c968289
luoruiqing <lrq184187664@163.com>
2017-06-14 16:02:32 +0800

    debug状态下打印sql

--- common/db_sum.py
+++ common/db_sum.py
@@ -1,5 +1,19 @@
 # -*- coding: utf-8 -*-
 
-#Create a unified handle to prevent the creation of multiple links to the database
+# Create a unified handle to prevent the creation of multiple links to the database
 from hillinsight.storage.db_conn import _mysql_config
+from conf.default import debug
+
 _metric_meta_db = _mysql_config['god_metric_meta']['master']['offline']
+
+if debug:  # debug状态下打印sql
+    query = _metric_meta_db.query
+
+
+    def print_sql(sql, *args, **kwargs):
+        """ 负责SQL打印 在debug状态下"""
+        print "[SQL]: ", sql
+        return query(sql, *args, **kwargs)
+
+
+    _metric_meta_db.query = print_sql

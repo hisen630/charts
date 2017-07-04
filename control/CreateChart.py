2b61a1ab048d88f8ddc022fa89a033cc7fc83165
luoruiqing <lrq184187664@163.com>
2017-07-04 17:26:03 +0800

    创建图表接口层

--- control/CreateChart.py
+++ control/CreateChart.py
@@ -1,7 +1,13 @@
 # coding:utf-8
+from __future__ import unicode_literals
+from __base__ import ChartsError
 from json import dumps
-from flask import request
 from route import CustomView
+from common.logger import logger
+from flask import request, jsonify
+from common.base import render_custom
+from manager.oper_m import OperManager
+from manager.source_m import SourceManager
 from manager.create_chart_m import CreateChartManager
 
 
@@ -9,25 +15,44 @@ class CreateChart(CustomView):
     def __init__(self):
         self.manager = CreateChartManager()
 
-    def preview(self):
+    def index(self):
+        dbs = SourceManager.get_data()
+        filters = OperManager.get_data()
+        return render_custom("/createchart/createchart.tpl", dbs=dbs, filters=filters)
+
+    def preview(self):  # TODO 必须post提交方式
         # http://127.0.0.1:16688/createchart/preview?field_id=1&cid=1&rid=1
         # if request.method != "POST":
         #     return Response("请用POST方式提交.", status=400)
+
         json = {
-            "field_id": 1,
+            "source_id": 1,
             "rows": [
-                {"name": "gmv", "oid": 1},
-                {"name": "month_sale", "oid": 1}
+                {"field": "gmv", "oid": 1},
+                # {"field": "month_sale", "oid": 1}
             ],
             "columns": [
-                {"name": "fg_category2_name.raw", "oid": 1, "size": 5, "order": "desc"},
-                # {"name": "fg_category3_name.raw", "oid": 2, "type": "terms", "size": 5, "order": "ase"}
+                {"field": "fg_category2_name.raw", "size": 5, "order": "desc"},
+                # {"field": "fg_category4_name.raw", "size": 5, "order": "desc"},
+                # {"field": "category2_id", "ranges": [(50510002, 50510003), (60510003, 60510004)]},
+                # {"field": "data_test", "type": "date", "interval": 15, "units": "hourly"},
 
             ],
-            "query": "*",
+            "filters": [
+                {"field": "task_date"}
+            ],
+            "query": 'fg_category2_name.raw:"零食/坚果/特产"',
+            "limit": 0  # 0代表所有
         }
-        # json = request.get_json(force=True)
+        json = request.get_json(force=True)
         json["type"] = 4  # 强制类型为es类型
-        json["query"] = "*"
-        print "request json:", dumps(json)
-        return dumps(self.manager.preview(**json))
+        json["filters"] = []  # 默认添加时间限制
+        logger._print("request json:" + dumps(json))
+        try:
+            result = {'status': True, "code": 0, 'msg': u'获取数据完成.', "data": self.manager.preview(**json)}
+        except ChartsError, e:
+            result = {"status": e.status, "code": e.code, "msg": e.message, "data": None}
+        return jsonify(result)
+
+    def save(self):
+        return dumps(self.manager.save())

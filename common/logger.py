3a3ed6b9a7614d514845ad0952c7dd7e9f2812fd
luoruiqing <lrq184187664@163.com>
2017-07-04 17:14:13 +0800

    调试日志必须在debug环境下才会打印

--- common/logger.py
+++ common/logger.py
@@ -1,6 +1,6 @@
 # coding:utf-8
 from sys import stdout
-from conf.default import LOG_LEVEL
+from conf.default import LOG_LEVEL, debug
 from logging import basicConfig, root as logger
 
 FORMAT = "[%(asctime)s - %(levelname)s]: %(message)s"
@@ -9,9 +9,10 @@ basicConfig(level=LOG_LEVEL, format=FORMAT, datefmt='%Y-%m-%d %H:%M:%S', stream=
 
 def _print(*args, **kwargs):
     """ 调试方法 前后虚线隔开"""
-    logger.debug("=" * 79)
-    logger.debug(*args, **kwargs)
-    logger.debug("=" * 79)
+    if debug:
+        logger.debug("=" * 79)
+        logger.debug(*args, **kwargs)
+        logger.debug("=" * 79)
 
 
 logger._print = _print

3d26ebca90d89aee74575bcadeee97aa62d05406
luoruiqing <lrq184187664@163.com>
2017-07-04 16:40:56 +0800

    父类参数重整

--- __base__/real_time_calculation.py
+++ __base__/real_time_calculation.py
@@ -17,20 +17,27 @@ class RealTimeCalculationMappingBase(RealTimeCalculation, Mapping):
     """ 实时计算基础方法并对应映射层 """
 
 
+# 这里可以实现嵌套类
+
 class RealTimeCalculationMapping(RealTimeCalculationMappingBase):
     """ 实时计算实现类统一接口 """
     address = None  # 地址
     rows = ()  # 行／维度
     columns = ()  # 列/指标
     query = "*"  # 查询内容默认为 *
+    filters = ()  # 过滤体条件，可以是多个
+    limit = 0  # 返回结果集条数
 
-    def __init__(self, address=None, columns=None, rows=None, query=None):
+    def __init__(self, address=None, query=None, filters=None,
+                 columns=None, rows=None, limit=None):
         # field_id, columns = None, rows = None, query = "*", type = 4
         """ 优先使用传入参数，否则使用默认参数 """
         self.address = address or self.address
-        self.columns = columns or self.columns
-        self.rows = rows or self.rows
         self.query = query or self.query
+        self.filters = filters or self.filters
+        self.rows = rows or self.rows
+        self.columns = columns or self.columns
+        self.limit = limit or self.limit
         assert self.address and self.query  # 索引、类型和query必须存在
 
 

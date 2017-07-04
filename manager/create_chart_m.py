f5365489085fa510e6897cf1880d0259de0faf0a
luoruiqing <lrq184187664@163.com>
2017-07-04 17:24:40 +0800

    选择映射时的逻辑更改

--- manager/create_chart_m.py
+++ manager/create_chart_m.py
@@ -1,9 +1,10 @@
 # coding:utf-8
 """  创建图表 """
-from __base__ import APIError
+from __base__ import APIError, NotImplementedManagerMappingTypeError, AssertionError
 from json import loads
 from source_m import SourceManager
 from common.base import get_module_object
+from modules.create_chart import mappings
 from manager.oper_m import OperManager
 
 
@@ -12,25 +13,24 @@ class CreateChartManager:
     _modules_split = 'create_chart'
 
     @classmethod
-    def get_model(cls, types):
-        objects = get_module_object(cls._modules_split)
-        for item in objects:
-            if objects[item].types == types:
-                return objects[item]
-        raise NotImplemented("未实现的映射方法")
-
-    @classmethod
     def preview(cls, **kwargs):
         """ 预览界面 
             参数检查和字段核对在这里完成
         """
-        source_info = SourceManager.get_by_id(kwargs.pop("field_id"))
+        source_info = SourceManager.get_by_id(kwargs.pop("source_id"))
         if not source_info:
-            raise APIError("数据不存在.")
-        req_fields = dict((item["name"], item) for item in kwargs["columns"] + kwargs["rows"])
-        for field in loads(source_info.get("columns")) or []:
+            raise APIError("该数据不存在.")
+        all_field = kwargs["columns"] + kwargs["rows"]
+        req_fields = dict((item["field"], item) for item in all_field)
+        for field in loads(source_info.get("columns", "[]")):
             req_fields.get(field["field"], {})["type"] = field["type"]
-        # raise APIError("该行列({})不存在预置数据中.".format(item["name"]))
+        for item in all_field:
+            assert "type" in item, "该字段({})不存在预置数据中.".format(item["field"])
 
-        return cls.get_model(source_info.pop("type", kwargs.pop("type"))).preview(  # 默认es
+        module = mappings[source_info.pop("type", kwargs.pop("type", 4))](
             **dict(kwargs, **{"address": source_info["address"]}))
+        return module.get_data()
+
+    @classmethod
+    def save(cls, **kwargs):
+        raise NotImplementedManagerMappingTypeError()

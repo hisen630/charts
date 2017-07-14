4f88dc903e01fc7b0d1e6d3884ec5b05e6293d02
luoruiqing <lrq184187664@163.com>
2017-07-14 11:38:47 +0800

    数据层，修改源数据

--- base/source_b.py
+++ base/source_b.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from common.db_sum import _metric_meta_db
 from common.sql_tools import Where
 
@@ -12,3 +13,14 @@ class SourceBase:
             where = "WHERE {};".format(where)
 
         return _metric_meta_db.query("SELECT * FROM `t_chart_source` {};".format(where)) or []
+
+    @staticmethod
+    def update(id, index, dimension, address_label):
+        return _metric_meta_db.query(
+            "UPDATE `t_chart_source` SET `address_label` = $address_label, `index` = $index, `dimension` = $dimension WHERE `id` = $id ;",
+            {
+                "id": id,
+                "address_label": address_label,
+                "index": index,
+                "dimension": dimension,
+            })
